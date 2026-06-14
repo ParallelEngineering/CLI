1b5073bed98e5b9d7e561d980ae681eadc352aa5
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2026-06-14 23:32:16 +0200

    init

---
+++ src/io/cli.h
@@ -0,0 +1,18 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <string>
+
+namespace cli {
+void help();
+
+namespace keyManager {
+void create();
+
+void list();
+
+void print(const std::string &name, bool publicKey, bool privateKey);
+}  // namespace keyManager
+}  // namespace cli
+
+#endif
